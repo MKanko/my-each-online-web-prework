01441b76a7f7169b2ee8847642b06258bfd8074f
Mark Kanko <mkanko66@gmail.com>
2019-03-19 06:00:19 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,15 +1,8 @@
 def my_each(collection)
-  
   item = 0 
-  
   while item < collection.length 
-    
     yield collection[item]
-      
-    item = item + 1 
-    
+      item = item + 1 
   end 
-  
   collection 
-  
 end
